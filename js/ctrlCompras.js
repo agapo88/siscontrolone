cf20da0fbde12ab1baa0feb3abe7294696c448d9
Agapito Xuruc <aga.xuruc88@gmail.com>
2016-10-20 21:01:15 -0600

    Finalización Guardar Productos con precio en Dolares

--- js/ctrlCompras.js
+++ js/ctrlCompras.js
@@ -80,6 +80,33 @@ miApp.controller('ctrlCompras', function($scope, $http, $alert, $filter){
 		$scope.compras.lstProductos.splice( index, 1);
 	}
 
+	$scope.resetObject = function( cod ){
+		$scope.detalleCompra = {};
+
+		if( cod == 1 ){
+			$scope.compras = {
+				idMoneda : 1,
+				noFactura    : null,
+				fechaIngreso : null,
+				totalDolares : 0,
+				idProveedor  : "1",
+				lstProductos : [],
+			};
+		}
+
+		if( cod == 2 ){
+			$scope.comprasD = {
+				idMoneda : 2,
+				noFactura    : null,
+				fechaIngreso : null,
+				totalDolares : 0,
+				idProveedor  : "1",
+				lstProductos : [],
+			};
+		}
+	}
+
+
 	// AGREGAR TOTAL
 	$scope.detalleCompra = {};
 	$scope.addCompra = function( idTipoMoneda ){
@@ -110,16 +137,33 @@ miApp.controller('ctrlCompras', function($scope, $http, $alert, $filter){
 		}
 
 		// VALIDACIÓN QUETZALES
-		if ( $scope.compras.idTipoMoneda == 1 ){
+		if ( idTipoMoneda == 1 ){
 			if( $scope.compras.lstProductos.length > 0 ){
+				console.log( "mayor que 0 " );
 				if( !($scope.compras.totalQuetzales >= $scope.subTotalQuetzales() + (detalleCompra.cantidad * detalleCompra.precioUnitario) ) ){
 					error = true;
 					$alert({title: 'Alerta: ', content: 'No tiene disponibilidad de dinero en quetzales.', placement: 'top', type: 'danger', show: true, duration: 5});
 				}
 			}else{
+				console.log( "Sin Datos " );
 				if( !($scope.compras.totalQuetzales >= (detalleCompra.cantidad * detalleCompra.precioUnitario) ) ){
 					error = true;
 					$alert({title: 'Alerta: ', content: 'La cantidad ingresada supera el monto disponible en quetzales.', placement: 'top', type: 'danger', show: true, duration: 5});
+				}
+			}
+		}
+
+		// VALIDACIÓN DOLARES
+		if ( idTipoMoneda == 2 ){
+			if( $scope.comprasD.lstProductos.length > 0 ){
+				if( !($scope.comprasD.totalDolares >= $scope.subTotalDolares() + (detalleCompra.cantidad * detalleCompra.precioUnitario) ) ){
+					error = true;
+					$alert({title: 'Alerta: ', content: 'No tiene disponibilidad de dinero en dolares.', placement: 'top', type: 'danger', show: true, duration: 5});
+				}
+			}else{
+				if( !($scope.comprasD.totalDolares >= (detalleCompra.cantidad * detalleCompra.precioUnitario) ) ){
+					error = true;
+					$alert({title: 'Alerta: ', content: 'La cantidad ingresada supera el monto disponible en dolares.', placement: 'top', type: 'danger', show: true, duration: 5});
 				}
 			}
 		}
@@ -128,14 +172,26 @@ miApp.controller('ctrlCompras', function($scope, $http, $alert, $filter){
 		if( !error ){
 			var fechaCaducidad = $filter('date')($scope.detalleCompra.fechaCaducidad, "yyyy-MM-dd");
 
-			// AGREGA AL ARREGLO LOS VALORES DEL OBJETO
-			$scope.compras.lstProductos.push({
-				idProducto     : detalleCompra.idProducto,
-				cantidad       : detalleCompra.cantidad,
-				precioUnitario : detalleCompra.precioUnitario,
-				idProveedor    : $scope.compras.idProveedor,
-				fechaCaducidad : fechaCaducidad
-			});
+			if( idTipoMoneda == 1 ){
+				// AGREGA AL ARREGLO LOS VALORES DEL OBJETO
+				$scope.compras.lstProductos.push({
+					idProducto     : detalleCompra.idProducto,
+					cantidad       : detalleCompra.cantidad,
+					precioUnitario : detalleCompra.precioUnitario,
+					idProveedor    : $scope.compras.idProveedor,
+					fechaCaducidad : fechaCaducidad
+				});
+			}
+			if( idTipoMoneda == 2 ){
+				// AGREGA AL ARREGLO LOS VALORES DEL OBJETO
+				$scope.comprasD.lstProductos.push({
+					idProducto     : detalleCompra.idProducto,
+					cantidad       : detalleCompra.cantidad,
+					precioUnitario : detalleCompra.precioUnitario,
+					idProveedor    : $scope.compras.idProveedor,
+					fechaCaducidad : fechaCaducidad
+				});		
+			}
 
 			$scope.detalleCompra = {};
 		}
@@ -155,8 +211,8 @@ miApp.controller('ctrlCompras', function($scope, $http, $alert, $filter){
 		$scope.itemDonador = {};
 	}
 
-	// GUARDAR DONADOR
-	$scope.guardarCompra = function(){
+	// GUARDAR COMPRAS QUETZALES
+	$scope.guardarCompraQ = function(){
 		var error = false;
 
 		if( !($scope.compras.noFactura && $scope.compras.noFactura > 0)  ){
@@ -179,7 +235,7 @@ miApp.controller('ctrlCompras', function($scope, $http, $alert, $filter){
 			.success(function(data){
 				console.log(data);
 				if( data.respuesta ){
-					//$scope.reset();
+					$scope.resetObject(1);
 					$alert({title: 'Mensaje: ', content: data.mensaje, placement: 'top', type: 'success', show: true, duration: 4});
 				}else{
 					$alert({title: 'Error: ', content: data.mensaje, placement: 'top', type: 'danger', show: true, duration: 4});
@@ -190,4 +246,39 @@ miApp.controller('ctrlCompras', function($scope, $http, $alert, $filter){
 		}
 	};
 	
+
+	// GUARDAR COMPRAS DOLARES
+	$scope.guardarCompraD = function(){
+		var error = false;
+
+		if( !($scope.comprasD.noFactura && $scope.comprasD.noFactura > 0)  ){
+			error = true;
+			$alert({title: 'Alerta: ', content: 'No ha ingresado un número de Factura.', placement: 'top', type: 'warning', show: true, duration: 4});
+		}
+		else if( !($scope.comprasD.fechaIngreso) ){
+			error = true;
+			$alert({title: 'Alerta: ', content: 'No ha seleccionado la fecha de ingreso.', placement: 'top', type: 'warning', show: true, duration: 4});
+		}
+		else if( !($scope.comprasD.lstProductos.length > 0 || $scope.comprasD.lstProductosD.length > 0 ) ){
+			error = true;
+			$alert({title: 'Alerta: ', content: 'No ha registrado ningún producto.', placement: 'top', type: 'warning', show: true, duration: 4});
+		}
+
+		// SI NO EXISTE ERROR
+		if( !error ){
+			var fechaIngreso = $filter('date')($scope.comprasD.fechaIngreso, "yyyy-MM-dd");
+			$http.post('consultas.php', {accion: 'guardarCompras', datos: $scope.comprasD, fechaIngreso: fechaIngreso})
+			.success(function(data){
+				console.log(data);
+				if( data.respuesta ){
+					$scope.resetObject(2);
+					$alert({title: 'Mensaje: ', content: data.mensaje, placement: 'top', type: 'success', show: true, duration: 4});
+				}else{
+					$alert({title: 'Error: ', content: data.mensaje, placement: 'top', type: 'danger', show: true, duration: 4});
+				}
+			}).error(function(data){
+				console.log(data);
+			});
+		}
+	};
 });
--- views/form.compras.php
+++ views/form.compras.php
@@ -18,14 +18,15 @@
       </div>
       <div class="form-group text-right">
          <div class="col-sm-12">
-            <h3 ng-show="idTipoMoneda==1">
-               TOTAL: Q.{{ compras.totalQuetzales - subTotalQuetzales() | number: 2 }} 
+            <h3 class="h3" ng-show="idTipoMoneda==1">
+               DISPONIBLE: Q.{{ compras.totalQuetzales - subTotalQuetzales() | number: 2 }} 
             </h3>
-            <h3 ng-show="idTipoMoneda==2">
-               TOTAL: $.{{ comprasD.totalDolares - subTotalDolares() | number: 2 }} 
+            <h3 class="h3" ng-show="idTipoMoneda==2">
+               DISPONIBLE: $.{{ comprasD.totalDolares - subTotalDolares() | number: 2 }} 
             </h3>
          </div>
       </div>
+      <!-- QUETZALES -->
       <form class="form-horizontal" novalidate autocomplete="off" ng-show="idTipoMoneda==1">
          <div class="form-group">
             <label class="control-label col-sm-3">
@@ -54,7 +55,6 @@
                </select>
             </div>
          </div>
-
          <!-- SELECCIONAR PRODUCTOS -->
          <div class="form-group" style="margin-left: 5px; margin-right: 5px">
             <label class="label label-default">Agregar compras</label>
@@ -87,7 +87,7 @@
                         <input type="text" class="form-control" ng-model="detalleCompra.fechaCaducidad" data-date-format="dd/MM/yyyy" data-date-type="number" data-min-date="today" data-autoclose="1" bs-datepicker ng-disabled="!bloquearFecha">
                      </td>
                      <td class="text-center">
-                        <button type="button" class="btn btn-xs btn-success" ng-click="addCompra()">
+                        <button type="button" class="btn btn-xs btn-success" ng-click="addCompra( 1 )">
                            <span class="glyphicon glyphicon-plus"></span>
                         </button>
                      </td>
@@ -162,7 +162,150 @@
          </div>
          <div class="form-group" style="margin-top: 20px">
             <div class="col-sm-12 text-right">
-               <button type="button" class="btn btn-success btn-lg noBorder" ng-click="guardarCompra()">
+               <button type="button" class="btn btn-success btn-lg noBorder" ng-click="guardarCompraQ()">
+                  <i class="glyphicon glyphicon-saved"></i> Guardar Compra
+               </button>
+            </div>
+         </div>
+      </form>
+      <!-- DOLARES -->
+      <form class="form-horizontal" novalidate autocomplete="off" ng-show="idTipoMoneda==2">
+         <div class="form-group">
+            <label class="control-label col-sm-3">
+               No. Factura
+            </label>
+            <div class="col-sm-3 col-md-2">
+               <input type="number" class="form-control" id="numeroFactura" ng-model="comprasD.noFactura">
+            </div>
+         </div>
+         <div class="form-group">
+            <label class="control-label col-sm-3">Fecha Adquisición:</label>
+            <div class="col-sm-3 col-md-2">
+               <div class="input-group">
+                  <span class="input-group-addon">
+                     <i class="glyphicon glyphicon-calendar"></i>
+                  </span>
+                  <input type="text" name="fechaDonacion" class="form-control" ng-model="comprasD.fechaIngreso" data-date-format="dd/MM/yyyy" data-date-type="number"  data-max-date="today" data-autoclose="1"  bs-datepicker>
+               </div>
+            </div>
+            <label class="control-label col-sm-2 col-md-2">Proveedor:</label>
+            <div class="col-sm-4 col-md-4">
+               <select class="form-control" ng-model="comprasD.idProveedor">
+                  <option value="{{ proveedor.idProveedor }}" ng-repeat="proveedor in lstProveedores">
+                     {{ proveedor.proveedor }} 
+                  </option>
+               </select>
+            </div>
+         </div>
+
+         <!-- SELECCIONAR PRODUCTOS -->
+         <div class="form-group" style="margin-left: 5px; margin-right: 5px">
+            <label class="label label-default">Agregar compras</label>
+            <table class="table table-striped table-condensed">
+               <thead>
+                  <tr>
+                     <th class="col-sm-3 text-center">compras</th>
+                     <th class="col-sm-3 text-center">Cantidad</th>
+                     <th class="col-sm-3 text-center">Precio Unitario</th>
+                     <th class="col-sm-2 text-center">Fecha Caducidad</th>
+                     <th class="text-center">Agregar</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr>
+                     <td>
+                        <select class="form-control" ng-model="detalleCompra.idProducto">
+                           <option value="{{ producto.idProducto }}" ng-repeat="producto in lstProductos">
+                              {{producto.producto}}
+                           </option>
+                        </select>
+                     </td>
+                     <td class="text-center">
+                        <input type="number" class="form-control" ng-model="detalleCompra.cantidad">
+                     </td>
+                     <td class="text-left">
+                        <input type="number" class="form-control" ng-model="detalleCompra.precioUnitario">
+                     </td>
+                     <td class="text-left">
+                        <input type="text" class="form-control" ng-model="detalleCompra.fechaCaducidad" data-date-format="dd/MM/yyyy" data-date-type="number" data-min-date="today" data-autoclose="1" bs-datepicker ng-disabled="!bloquearFecha">
+                     </td>
+                     <td class="text-center">
+                        <button type="button" class="btn btn-xs btn-success" ng-click="addCompra( 2 )">
+                           <span class="glyphicon glyphicon-plus"></span>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr> 
+                     <td colspan="5" class="text-right">
+                        <!-- CANCELAR AGREGAR compras -->
+                        <button type="button" class="btn btn-danger btn-sm noBorder" ng-click="resetObject()">
+                           CANCELAR
+                        </button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+         <hr>
+         <div class="form-group text-center">
+            <legend>LISTADO DE PRODUCTOS</legend>
+         </div>
+         <div class="form-group" style="margin-left: 5px; margin-right: 5px">
+            <label class="label label-info">Listado de Productos</label>
+            <table class="table table-striped">
+               <thead>
+                  <tr>
+                     <th class="text-center">No.</th>
+                     <th class="col-sm-3 text-center">Producto</th>
+                     <th class="col-sm-3 text-center">Proveedor</th>
+                     <th class="col-sm-1 text-center">Cantidad</th>
+                     <th class="col-sm-1 text-center">Precio Unitario</th>
+                     <th class="col-sm-2 text-center">Fecha Caducidad</th>
+                     <th class="text-center">Total</th>
+                     <th></th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <!-- QUETZALES -->
+                  <tr ng-repeat="(ixProd, prod) in comprasD.lstProductos" ng-show="comprasD.lstProductos.length > 0">
+                     <td>{{ prod.idProducto }}</td>
+                     <td class="text-center">
+                        <select class="selectpicker" ng-model="prod.idProducto" disabled>
+                           <option value="{{ producto.idProducto }}" ng-repeat="producto in lstProductos">
+                              {{producto.producto}}
+                           </option>
+                        </select>
+                     </td>
+                     <td class="text-center">
+                        <select class="selectpicker" ng-model="prod.idProveedor" disabled>
+                           <option value="{{ proveedor.idProveedor }}" ng-repeat="proveedor in lstProveedores">
+                              {{ proveedor.proveedor }} 
+                           </option>
+                        </select>
+                     </td>
+                     <td class="text-center">{{ prod.cantidad }}</td>
+                     <td class="text-right">{{ prod.precioUnitario | number: 2 }}</td>
+                     <td class="text-center">{{ prod.fechaCaducidad | date :  "dd/MM/y" }}</td>
+                     <td class="text-right">{{ prod.cantidad * prod.precioUnitario | number: 2 }}</td>
+                     <td class="text-center">
+                        <button type="button" class="btn btn-danger btn-xs" ng-click="deleteProdQuetzales( ixProd )">
+                           <span class="glyphicon glyphicon-remove"></span>
+                        </button>
+                     </td>
+                  </tr>
+                  <tr id="tb-title" ng-show="compras.lstProductos.length > 0">
+                     <td colspan="7" class="text-right">
+                        <strong> SUBTOTAL {{ subTotalQuetzales() | number: 2 }}</strong>
+                     </td>
+                     <td></td>
+                  <td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+         <div class="form-group" style="margin-top: 20px">
+            <div class="col-sm-12 text-right">
+               <button type="button" class="btn btn-success btn-lg noBorder" ng-click="guardarCompraQ()">
                   <i class="glyphicon glyphicon-saved"></i> Guardar Compra
                </button>
             </div>

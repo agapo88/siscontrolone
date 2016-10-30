c19483f6eb2ca93c2bda5e12cd4080a821419247
agapo88 <aga.xuruc88@gmail.com>
2016-10-29 18:56:01 -0600

    Se agregó Modelo ER en el proyecto

--- views/inicio.php
+++ views/inicio.php
@@ -1,1 +1,11 @@
-<h1>BIENVENIDO</h1>
+<div class="row">
+
+	<div class="col-sm-4 col-md-3 col-lg-2">
+		<div class="panel-heading">
+			<h3>
+				<span class="glyphicon glyphicon-user"></span> Familias
+			</h3>
+		</div>
+	</div>
+	
+</div>
